68e73244ce57834076e76861f35bdb4fdf4afd02
zero.qn <zero.qn@outlook.com>
2022-03-07 16:39:27 +0800

    feat(assignment1): commit game item to merkle tree

--- assignment-1/nft/contracts/GameItem.sol
+++ assignment-1/nft/contracts/GameItem.sol
@@ -7,16 +7,26 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "./SimpleMerkleTree.sol";
+
 contract GameItem is ERC721URIStorage {
     using Counters for Counters.Counter;
     using Strings for uint256;
-    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("GameItem", "ITM") {}
+    Counters.Counter private _tokenIds;
+    SimpleMerkleTree4 merkleTree;
+
+    constructor() ERC721("GameItem", "ITM") {
+        merkleTree = new SimpleMerkleTree4();
+        bytes32 zeroRoot = hex"890740a8eb06ce9be422cb8da5cdafc2b58c0a5e24036c578de2a433c828ff7d";
+        assert(merkleTree.root() == zeroRoot);
+    }
 
     function awardItem(address player) public returns (uint256) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
+        require(newItemId < 5, "reach max game item id");
+        require(newItemId > 0, "minimal id is 1");
 
         // Here we encode raw json erc721 metadata string into a base64
         // string and use it as TokenURI.
@@ -37,6 +47,16 @@ contract GameItem is ERC721URIStorage {
         _mint(player, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
+        bytes32 leafCom = keccak256(
+            abi.encodePacked(msg.sender, player, newItemId, tokenURI)
+        );
+        // Aka id 1 => merkle leaf index 0 and id 2 => 1 merkle leaf index 1
+        merkleTree.update(leafCom, newItemId - 1);
+
         return newItemId;
+    }
+
+    function merkleRoot() public view returns (bytes32) {
+        return merkleTree.root();
     }
 }
--- assignment-1/nft/test/game-item.js
+++ assignment-1/nft/test/game-item.js
@@ -10,16 +10,33 @@ describe("GameItem", function () {
     const players = await ethers.getSigners();
     const awardItemTx = await gameItem.awardItem(players[0].address);
     await awardItemTx.wait();
-    
+
     expect(await gameItem.ownerOf(1)).to.equal(players[0].address);
-    
+
     const tokenURI = await gameItem.tokenURI(1);
     const [contentType, contentBase64String] = tokenURI.split(",");
     expect(contentType).to.equal("data:application/json;base64");
-    
-    const dataURIString = new TextDecoder().decode(ethers.utils.base64.decode(contentBase64String));
+
+    const dataURIString = new TextDecoder().decode(
+      ethers.utils.base64.decode(contentBase64String)
+    );
     const dataURI = JSON.parse(dataURIString);
     expect(dataURI.name).to.equal("GameItem #1");
     expect(dataURI.description).to.equal("demo game item nft");
+  });
+
+  it("Should commit the new awarded item to merkle tree", async function () {
+    const GameItem = await ethers.getContractFactory("GameItem");
+    const gameItem = await GameItem.deploy();
+    await gameItem.deployed();
+
+    const players = await ethers.getSigners();
+    const awardItemTx = await gameItem.awardItem(players[0].address);
+    await awardItemTx.wait();
+
+    expect(await gameItem.ownerOf(1)).to.equal(players[0].address);
+    expect(await gameItem.merkleRoot()).to.equal(
+      "0x991f21582ee0eede856f8c55436020861c40ef0035b831660f915c39e17a078d"
+    );
   });
 });
