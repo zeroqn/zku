e43b035ee67a77b00c11efa881a0abffcced5eec
zero.qn <zero.qn@outlook.com>
2022-03-07 15:59:25 +0800

    feat(assignment1): add SimpleMerkleTree contract

---
+++ assignment-1/nft/contracts/SimpleMerkleTree.sol
@@ -0,0 +1,61 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract SimpleMerkleTree4 {
+    // 1 root + 2 branches + 4 leaves
+    bytes32[7] nodes;
+    uint256 leafOffset = 3;
+    uint256 branchOffset = 1;
+
+    // Construct a merkle tree use zero as leaf
+    constructor() {
+        // Calculate leaf hashes
+        for (uint256 i = 0; i < 4; i++) {
+            nodes[i + leafOffset] = keccak256(abi.encodePacked(uint256(0)));
+        }
+
+        // Calculate branch hashes
+        for (uint256 i = 0; i < 2; i++) {
+            bytes32 left = nodes[i * 2 + leafOffset];
+            bytes32 right = nodes[i * 2 + leafOffset + 1];
+            nodes[i + branchOffset] = keccak256(abi.encodePacked(left, right));
+        }
+
+        nodes[0] = keccak256(abi.encodePacked(nodes[1], nodes[2]));
+    }
+
+    function zeroHash() public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(uint256(0)));
+    }
+
+    function root() public view returns (bytes32) {
+        return nodes[0];
+    }
+
+    function update(bytes32 leaf, uint256 leafIndex) public returns (bytes32) {
+        require(leafIndex < 4, "leaf index out of bound");
+
+        // Update leaf
+        uint256 index = leafIndex + leafOffset;
+        nodes[index] = keccak256(abi.encodePacked(leaf));
+
+        while (0 != index) {
+            uint256 parentIndex = (index - 1) / 2;
+            if ((index - 1) % 2 == 0) {
+                // Left
+                nodes[parentIndex] = keccak256(
+                    abi.encodePacked(nodes[index], nodes[index + 1])
+                );
+            } else {
+                // Right
+                nodes[parentIndex] = keccak256(
+                    abi.encodePacked(nodes[index - 1], nodes[index])
+                );
+            }
+
+            index = parentIndex;
+        }
+
+        return nodes[0];
+    }
+}
---
+++ assignment-1/nft/test/simple-merkle-tree.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SimpleMerkleTree", function () {
+  it("Should able to update merkle root", async function () {
+    const SimpleMerkleTree = await ethers.getContractFactory(
+      "SimpleMerkleTree4"
+    );
+    const merkleTree = await SimpleMerkleTree.deploy();
+    await merkleTree.deployed();
+
+    const zeroRoot = await merkleTree.root();
+    expect(zeroRoot).to.equal(
+      "0x890740a8eb06ce9be422cb8da5cdafc2b58c0a5e24036c578de2a433c828ff7d"
+    );
+
+    // Try default value
+    const abiCoder = new ethers.utils.AbiCoder();
+    const leaf = abiCoder.encode(["uint256"], [0]);
+    expect(await merkleTree.zeroHash()).to.equal(ethers.utils.keccak256(leaf));
+
+    for (let i = 0; i < 4; i++) {
+      const updateLeafTx = await merkleTree.update(leaf, i);
+      await updateLeafTx.wait();
+      expect(await merkleTree.root()).to.equal(zeroRoot);
+    }
+
+    const leafOne = abiCoder.encode(["uint256"], [1]);
+    const updateLeafTx = await merkleTree.update(leafOne, 1);
+    await updateLeafTx.wait();
+    expect(await merkleTree.root()).to.equal(
+      "0x6f21267e2924835775d03cf48818214cc95760e04b05cfe0320a33f5a5883d59"
+    );
+  });
+});
